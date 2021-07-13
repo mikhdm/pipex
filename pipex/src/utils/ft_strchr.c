b46ddaf3b5cf5d068909ba937a6f507e7f37f11c
Rafael Mander <rmander@il-e3.msk.21-school.ru>
2021-07-13 23:49:38 +0300

    add strchr

--- pipex/src/utils/ft_strchr.c
+++ pipex/src/utils/ft_strchr.c
@@ -6,8 +6,21 @@
 /*   By: rmander <rmander@student.21-school.ru      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 23:30:10 by rmander           #+#    #+#             */
-/*   Updated: 2021/07/13 23:30:11 by rmander          ###   ########.fr       */
+/*   Updated: 2021/07/13 23:49:01 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char*)s);
+		++s;
+	}
+	if (*s == (char)c)
+		return ((char*)s);
+	return (NULL);
+}
